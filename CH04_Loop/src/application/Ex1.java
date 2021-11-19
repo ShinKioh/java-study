584ba67cfc319ba4ada0170b4ff3941d85f201f6
[sko] <[asdasd1990@naver.com]>
2021-11-19 12:04:12 +0900

    11월19일 오전

--- CH04_Loop/src/application/Ex1.java
+++ CH04_Loop/src/application/Ex1.java
@@ -8,10 +8,11 @@ public class Ex1 {
 		System.out.println("옵션을 선택 > ");
 		
 		Scanner scanner = new Scanner(System.in);
-		
 		int option = scanner.nextInt();
+		scanner.close();
 		
-		if (option == 1) {
+		/*
+		if (option == 1) { // ifElseIfElse문 사용
 			System.out.println("헬로우");
 		}
 		else if (option == 2) {
@@ -25,6 +26,26 @@ public class Ex1 {
 		}
 		else {
 			System.out.println("잘못된 옵션입니다.");
+		}
+		*/
+		
+		
+		switch (option){ // switch문 사용
+		case 1:
+			System.out.println("헬로우");
+			break;
+		case 2:
+			System.out.println("안녕 ?");
+			break;
+		case 3:
+			System.out.println("프로그램 종료");
+			break;
+		case 10000:
+			System.out.println("이스터에그를 발견했습니다");
+			break;
+		default:	
+			System.out.println("잘못된 옵션입니다");
+			break;
 		}
 	}
 
