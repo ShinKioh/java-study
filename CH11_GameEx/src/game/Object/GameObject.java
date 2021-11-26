891012fa744ddad37b4bc8221c182207b15fd414
[sko] <[asdasd1990@naver.com]>
2021-11-26 16:40:34 +0900

    11.26 오후 커밋

--- CH11_GameEx/src/game/Object/GameObject.java
+++ CH11_GameEx/src/game/Object/GameObject.java
@@ -4,6 +4,17 @@ public class GameObject {
 	private int id; // 아이디
 	private String name; // 이름
 	
+	private int[][] comparison = { 
+	//				 가위 바위 보
+	/*가위(0)*/		{ 0, -1, 1 }, 
+	/*바위(1)*/		{ 1, 0, -1 }, 
+	/*보 (2)*/		{ -1, 1, 0 }
+	};
+	// 상대의 (가위,바위,보)와 비교해서 결과 리턴
+	public int compareTo(GameObject ob) {
+		return comparison[id][ob.id];
+	}
+	
 	protected int getId() { // 아이디 수정은 상속한 클래스에서만 사용
 		return id;
 	}
@@ -21,6 +32,7 @@ public class GameObject {
 	public String toString() {
 		return "GameObject [id=" + id + ", name=" + name + "]";
 	}
+	
 	
 	
 }
